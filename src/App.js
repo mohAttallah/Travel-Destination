8ceed440b702ef5423634ff689eb2bb1e4072c5c
mohAttallah <mohmohmoud28@gmail.com>
2023-05-07 03:12:37 +0000

    Add Route liks,  And more components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Home from "../src/components/home/home.js"
 import data from "../src/data/db.json";
+import '../src/App.css'
 
 function App() {
   return (
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import'../header/header.css';
 export default function Header() {
-    return (
-      <header>
-        <h1>Travel Destination</h1>
-      </header>
-    );
+  return (
+    <header>
+      <h1>Travel Destination</h1>
+      <Link to="/">Home</Link>
+    </header>
+  );
 }
